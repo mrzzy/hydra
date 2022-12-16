705e14ab182903ed1c28aa8df7c7d8adc8ef57cd
Zhu Zhanyan <program.nom@gmail.com>
2022-12-16 12:00:57 +0800

    feat(ui): add NoServers component to render graphic when no servers are registered.

--- ui/pages/index.tsx
+++ ui/pages/index.tsx
@@ -3,28 +3,66 @@
  * Pages
  * Homepage
  */
-import React from 'react'
-import { ConfigProvider, Layout } from 'antd';
+import React, { useEffect, useState } from 'react'
+import { Button, ConfigProvider, Layout, Space } from 'antd';
 import Image from 'next/image';
 import styles from 'styles/Home.module.css';
 import { Rye } from '@next/font/google'
 
-const LogoFont = Rye({weight: '400',});
+const DisplayFont = Rye({ weight: '400', });
 
+/**
+ * Renders a graphic to users when no servers are registered.
+ */
+function NoServers() {
+  return (
+    <div className={styles.noServers}>
+      <Image
+        src="/static/no_servers_graphic.png"
+        alt="No Servers"
+        height={450}
+        width={578}
+      />
+      <h1 className={`${DisplayFont.className} ${styles.noServersTitle}`}>No Servers</h1>
+      <Button type="primary">Add Server</Button>
+    </div>
+  );
+}
+
+/**
+ * Homepage renders a overview of proxy servers.
+ */
 export default function Home() {
+  interface Server {
+    isOnline: boolean
+    latencyMs: number
+    address: {
+      host: string
+      port: number
+    }
+    cipher: string
+  }
+  // initially, render with no servers,
+  const [servers, setServers] = useState([]);
+  // TODO: trigger an api call effect to fetch currently registered Servers
+
   return (
     <ConfigProvider theme={{
       token: {
-        colorPrimary: "#de2635",
+        colorPrimary: "#E34B4B",
       },
     }}>
       <Layout>
         <Layout.Header className={styles.header}>
           <div className={styles.logo}>
             <Image src="/static/hydra_logo_no_text.png" alt='logo' width={64} height={64} />
-            <span className={`${LogoFont.className} ${styles.logoTitle}`}>HYDRA</span>
+            <span className={`${DisplayFont.className} ${styles.logoTitle}`}>HYDRA</span>
           </div>
         </Layout.Header>
+
+        <Layout.Content>
+          {(servers.length <= 0) ? <NoServers /> : null}
+        </Layout.Content>
       </Layout>
     </ConfigProvider>
   )
